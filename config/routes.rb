456fab1eb594c65c6d2e0b39ab015c6b5491e8df
Konstantin Yatsenko <moahtdeep@gmail.com>
2021-11-09 16:28:59 +0900

    Added seeds and created articles controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  get '/articles', to: 'articles#index', as: 'articles'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
